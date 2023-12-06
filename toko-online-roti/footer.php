30ef6d6f963c8140337841501910492eec201648
nukman <120618688+nukman1234@users.noreply.github.com>
2023-12-06 15:46:50 +0700

    Add files via upload

---
+++ toko-online-roti/footer.php
@@ -0,0 +1,27 @@
+
+	<footer style="border-top: 4px solid #ff8680;  ">
+		<div class="container" style="padding-bottom: 50px;">
+			<div class="row">
+				<div class="col-md-4">
+					<h3 style="color: #ff8680"><b>NUKI-CAKE BAKERY</b></h3>
+					<p>Jl. Danau Kerinci V No.15</p>
+					<p><i class="glyphicon glyphicon-earphone"></i> +6285257321080</p>
+					<p><i class="glyphicon glyphicon-envelope"></i> nukicake@gmail.com</p>
+				</div>
+				<div class="col-md-4">
+					<h5><b>Menu</b></h5>
+					<p><a href=""  style="color: #000">Produk</a></p>
+					<p><a href=""  style="color: #000">Tentang kami</a></p>
+					<p><a href=""  style="color: #000">Hubungi Kami</a></p>
+				</div>
+
+				<div class="col-md-4">
+					
+				</div>
+			</div>
+
+		</div>
+	</footer>
+
+</body>
+</html>
